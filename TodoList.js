2a32db00680eee07b33c7acf2304e97d9bd99df2
priya-bhagat01 <pb337205@gmail.com>
2026-07-07 20:14:39 +0530

    Displays list on screen

--- TodoList.js
+++ TodoList.js
@@ -1,10 +1,24 @@
 const todoList = [];
+displayTodoList();
+
+function displayTodoList() {
+  let todoListHTML = '';
+     for (let i = 0; i < todoList.length; i++) {
+	   const todo = todoList[i];
+	   const html = `<p>${todo}</p>`;
+	   todoListHTML += html;
+       }
+  document.querySelector('.list')
+    .innerHTML = todoListHTML;
+}
+
 function addTodoList() {
 	const inputElement = document.querySelector('.input-button');
 	const name = inputElement.value
-	if (name.trim() != '') {
-		todoList.push(name);
-	console.log(todoList);
-	inputElement.value = '';
-    };	
+	  if (name.trim() != '') { //(prevents adding empty text)
+			todoList.push(name);
+	  console.log(todoList);
+	  inputElement.value = '';
+	  };
+    displayTodoList();
 }
